49b879c41071b2d4274a364b4a85379262a7adfd
Brandt Redd <brandt@redd.org>
2018-12-17 16:02:16 -0700

    Add DateTime parsing function for values in ExifTool format.

--- ExifToolWrapper.cs
+++ ExifToolWrapper.cs
@@ -3,9 +3,9 @@
 name: ExifToolWrapper.cs
 description: C# Wrapper for Phil Harvey's excellent ExifTool
 url: https://github.com/FileMeta/ExifToolWrapper/raw/master/ExifToolWrapper.cs
-version: 1.0
+version: 1.1
 keywords: CodeBit
-dateModified: 2018-11-02
+dateModified: 2018-12-16
 license: http://unlicense.org
 about: https://sno.phy.queensu.ca/~phil/exiftool/
 # Metadata in MicroYaml format. See http://filemeta.org/CodeBit.html
@@ -85,7 +85,7 @@ namespace ExifToolWrapper
             m_out = m_exifTool.StandardOutput;
         }
 
-        public void GetProperties(string filename, ICollection<KeyValuePair<string, string> > propsRead)
+        public void GetProperties(string filename, ICollection<KeyValuePair<string, string>> propsRead)
         {
             m_in.Write(filename);
             m_in.Write("\n-execute\n");
@@ -108,15 +108,15 @@ namespace ExifToolWrapper
                     {
                         string key = line.Substring(1, eq - 1);
                         string value = line.Substring(eq + 1).Trim();
-                        propsRead.Add(new KeyValuePair<string, string>(key, value));                      
+                        propsRead.Add(new KeyValuePair<string, string>(key, value));
                     }
                 }
             }
         }
 
-    #region IDisposable Support
+        #region IDisposable Support
 
-    protected virtual void Dispose(bool disposing)
+        protected virtual void Dispose(bool disposing)
         {
             if (m_exifTool != null)
             {
@@ -174,5 +174,55 @@ namespace ExifToolWrapper
         }
         #endregion
 
+        #region Static Methods
+
+        /// <summary>
+        /// Attempt to parse a date-time in the format used by ExifTool
+        /// </summary>
+        /// <param name="s">The string to be parsed</param>
+        /// <param name="kind">The <see cref="DateTimeKind"/> to be assigned to the resulting value. It is generally
+        /// determined by the definition of the corresponding field.</param>
+        /// <param name="date">The resulting parsed date.</param>
+        /// <returns>True if successful, else false.</returns>
+        /// <remarks>
+        /// <para>ExifTool formats dates as follows: "YYYY:MM:DD hh:mm:ss". For example, "2018:06:22 19:32:53".</para>
+        /// </remarks>
+        public static bool TryParseDate(string s, DateTimeKind kind, out DateTime date)
+        {
+            date = DateTime.MinValue;
+            int year, month, day, hour, minute, second;
+            s = s.Trim();
+            if (!int.TryParse(s.Substring(0, 4), out year)) return false;
+            if (s[4] != ':') return false;
+            if (!int.TryParse(s.Substring(5, 2), out month)) return false;
+            if (s[7] != ':') return false;
+            if (!int.TryParse(s.Substring(8, 2), out day)) return false;
+            if (s[10] != ' ') return false;
+            if (!int.TryParse(s.Substring(11, 2), out hour)) return false;
+            if (s[13] != ':') return false;
+            if (!int.TryParse(s.Substring(14, 2), out minute)) return false;
+            if (s[16] != ':') return false;
+            if (!int.TryParse(s.Substring(17, 2), out second)) return false;
+
+            if (year < 1900 || year > 2200) return false;
+            if (month < 1 || month > 12) return false;
+            if (day < 1 || day > 31) return false;
+            if (hour < 0 || hour > 23) return false;
+            if (minute < 0 || minute > 59) return false;
+            if (second < 0 || second > 59) return false;
+
+            try
+            {
+                date = new DateTime(year, month, day, hour, minute, second, 0, kind);
+            }
+            catch (Exception)
+            {
+                return false; // Probaby a month with too many days.
+            }
+
+            return true;
+        }
+
+        #endregion Static Methods
     }
 }
--- Program.cs
+++ Program.cs
@@ -43,6 +43,8 @@ report an error.";
 
                     ProcessFiles(arg);
                 }
+
+                TestDateParser();
             }
             catch (Exception err)
             {
@@ -92,6 +94,81 @@ report an error.";
             Console.WriteLine();
         }
 
+        static readonly string[] s_dateFailureTests = new string[]
+        {
+            "2018-12-17 15:26:23", // Dashes instead of colons on the date section
+            "2018:11:31 15:26:23", // November only has 30 days
+            "2017:02:29 15:26:23", // February only has 28 days
+            "2017:02:29 24:26:23", // No 24th hour
+        };
 
+    static readonly int[] s_dayLimit = new int[] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        static bool TestDateParser()
+        {
+            // Try a full decade
+            for (int year=1995; year <= 2004; ++year)
+            {
+                for (int month=1; month <= 12; ++month)
+                {
+                    int dayLimit = s_dayLimit[month];
+                    if (dayLimit == 28 && year % 4 == 0) dayLimit = 29;
+                    for (int day=1; day <= dayLimit; ++day)
+                    {
+                        if (!TestDateParser(year, month, day, month, day, month + day)) return false;
+                    }
+                }
+            }
+
+            // Try a full day
+            for (int hour=0; hour<24; ++hour)
+            {
+                for (int minute=0; minute<60; ++minute)
+                {
+                    for (int second=0; second<60; ++second)
+                    {
+                        if (!TestDateParser(2000 + hour + minute, hour % 12 + 1, minute % 28 + 1, hour, minute, second)) return false;
+                    }
+                }
+            }
+
+            foreach(var s in s_dateFailureTests)
+            {
+                if (!TestDateParserError(s)) return false;
+            }
+
+            Console.WriteLine("Date parsing tests passed.");
+            return true;
+        }
+
+        static bool TestDateParser(int year, int month, int day, int hour, int minute, int second)
+        {
+            string dtFormatted = string.Format("{0:D4}:{1:D2}:{2:D2} {3:D2}:{4:D2}:{5:D2}", year, month, day, hour, minute, second);
+            DateTime dtParsed;
+            if (!ExifTool.TryParseDate(dtFormatted, DateTimeKind.Unspecified, out dtParsed))
+            {
+                Console.WriteLine("Date Parse failed to parse: {0}", dtFormatted);
+                return false;
+            }
+
+            DateTime dtComposed = new DateTime(year, month, day, hour, minute, second, DateTimeKind.Unspecified);
+            if (!dtParsed.Equals(dtComposed))
+            {
+                Console.WriteLine("Date Parse doesn't match: {0}", dtFormatted);
+                return false;
+            }
+            return true;
+        }
+
+        static bool TestDateParserError(string failString)
+        {
+            DateTime dtParsed;
+            if (ExifTool.TryParseDate(failString, DateTimeKind.Unspecified, out dtParsed))
+            {
+                Console.WriteLine("Date parse should have failed: {0}", failString);
+                return false;
+            }
+            return true;
+        }
     }
 }
